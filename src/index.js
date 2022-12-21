e6905d628e7a7f97ac237138d4cd7042e569fa45
Daniil_Khobotov <dinich44@gmail.com>
2022-12-21 13:47:58 +0400

    add counter

--- src/index.js
+++ src/index.js
@@ -1,13 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { createStore } from 'redux';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
+const initialState = {value: 0};
+
+const reducer = (state = initialState, action) => {
+	switch (action.type) {
+		case 'INC':
+			return {
+				...state,
+				value: state.value + 1
+			};
+		case 'DEC':
+			return {
+				...state,
+				value: state.value - 1
+			};
+		case 'RND':
+			return {
+				...state,
+				value: state.value * action.payload
+			};
+		default:
+			return state;
+	}
+}
+
+const store = createStore(reducer);
+
+const update = () => {
+	document.getElementById('counter').textContent = store.getState().value;
+}
+
+store.subscribe(update);
+
+const inc = () => ({type: 'INC'});
+const dec = () => ({type: 'DEC'});
+const rnd = (value) => ({type: 'RND', payload: value});
+
+document.getElementById('inc').addEventListener('click', () => {
+	store.dispatch(inc());
+});
+
+document.getElementById('dec').addEventListener('click', () => {
+	store.dispatch(dec());
+});
+
+document.getElementById('rnd').addEventListener('click', () => {
+	const value = Math.floor(Math.random() * 10);
+	store.dispatch(rnd(value));
+});
+
 root.render(
-  <React.StrictMode>
-    <>
-    
-    </>
-  </React.StrictMode>
+	<React.StrictMode>
+		<>
+
+		</>
+	</React.StrictMode>
 );
 
